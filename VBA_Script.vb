7455785df0724ce7813cff1f008b73673ea88b1c
MohammedRizwan-1 <mohammedrizwannn@gmail.com>
2022-11-02 15:17:33 +0000

    Defining-Given-Variables

---
+++ VBA_Script.vb
@@ -0,0 +1,16 @@
+
+'Stock_Testing_VBA_Analsysis
+
+    Sub Stock_Testing_Data():
+
+        '1.0 Defining Given Variables in Sheet
+
+            Dim WorksheetName as String
+            Dim Ticker As String
+            Dim Stock_Volume As LongLong
+            Dim Yearly_Open As Double
+            Dim Yearly_Close As Double
+            Dim YR_Change As Double
+            Dim Percent_Change As Double
+            Dim Summary_Table_Row As Double
+            Dim Start_Row As Double
